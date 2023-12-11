45eb7cd2094161fea8d42c5f363f2898e5f27fcd
Antonella Arrieta <60509465+Tony-555@users.noreply.github.com>
2023-12-11 16:58:47 -0300

    Update FER-T.py

--- FER-T 1.0/FER-T.py
+++ FER-T 1.0/FER-T.py
@@ -1,8 +1,8 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.4),
-    on November 29, 2023, at 18:09
+    on December 01, 2023, at 11:06
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -54,7 +54,7 @@ filename = _thisDir + os.sep + 'data/%s_%s_%s' %(expInfo['participant'], expName
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\aarri\\Documents\\FERT-master\\FERT-master\\FER-T 1.0\\FER-T.py',
+    originPath='C:\\Users\\aarri\\555\\FER-T 1.0\\FER-T.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
@@ -67,15 +67,12 @@ frameTolerance = 0.001  # how close to onset before 'same' frame
 # Start Code - component code to be run after the window creation
 
 # --- Setup the Window ---
-win = visual.Window(
-    size=[1024, 768], fullscr=True, screen=0, 
-    winType='pyglet', allowStencil=False,
-    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
-    blendMode='avg', useFBO=True)
-win.mouseVisible = False
+# Create a PsychoPy window
+win = visual.Window([800, 600], fullscr=False, allowGUI=True, monitor='testMonitor', units='deg')
+
 # store frame rate of monitor if we can measure it
 expInfo['frameRate'] = win.getActualFrameRate()
-if expInfo['frameRate'] != None:
+if expInfo['frameRate'] is not None:
     frameDur = 1.0 / round(expInfo['frameRate'])
 else:
     frameDur = 1.0 / 60.0  # could not measure, so guess
@@ -96,7 +93,17 @@ defaultKeyboard = keyboard.Keyboard(backend='iohub')
 
 # --- Initialize components for Routine "Instr" ---
 text_2 = visual.TextStim(win=win, name='text_2',
-    text='Hola!! \n\n¿Estas pronto para seguir avanzando en el tablero? \n\n\nEn esta tarea veras imagenes de actores que expresan 3 emociones distintas (miedo, enojo, sorpresa, tristeza, felicidad y asco)\nAlgunas de estas expresiones son dificiles de detectar. \n¡Tu tarea es adivinar la emocion que los actores estan expresando!\n\n\nEn cada pantalla tu veras dos imagenes: a la izquierda una expresion neutra y a la derecha la imagen con la expreison que tiene que adivinar! \nApreta el numero que corresponde a la emocion o dila en voz alta\n\n\n¡Presiona la barra espaciadora para comenzar! \n\n',
+    text='¿Estas pronto para seguir avanzando en el tablero? \n\n\nEn esta tarea veras imagenes de actores que expresan 3 emociones distintas (miedo, enojo, sorpresa, tristeza, felicidad y asco)\n\n',
+    font='Arial',
+    pos=[0, 0], height=0.07, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp_3 = keyboard.Keyboard()
+
+# --- Initialize components for Routine "Instr" ---
+text_2 = visual.TextStim(win=win, name='text_2',
+    text='¿Estas pronto para seguir avanzando en el tablero? \n\n\nEn esta tarea veras imagenes de actores que expresan 3 emociones distintas (miedo, enojo, sorpresa, tristeza, felicidad y asco)\n\n',
     font='Arial',
     pos=[0, 0], height=0.07, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1, 
@@ -110,7 +117,7 @@ image = visual.ImageStim(
     win=win,
     name='image', 
     image='sin', mask=None, anchor='center',
-    ori=0, pos=[-0.5, 0], size=[0.7, 0.7],
+    ori=0, pos=[-0.5, 0], size=[0.35, 0.35],
     color=[1,1,1], colorSpace='rgb', opacity=1,
     flipHoriz=False, flipVert=False,
     texRes=128, interpolate=True, depth=-1.0)
@@ -118,7 +125,7 @@ image_2 = visual.ImageStim(
     win=win,
     name='image_2', 
     image='sin', mask=None, anchor='center',
-    ori=0, pos=[0.5, 0], size=[0.7, 0.7],
+    ori=0, pos=[0.5, 0], size=[0.35, 0.35],
     color=[1,1,1], colorSpace='rgb', opacity=1,
     flipHoriz=False, flipVert=False,
     texRes=128, interpolate=True, depth=-2.0)
@@ -126,23 +133,23 @@ key_resp_2 = keyboard.Keyboard()
 text = visual.TextStim(win=win, name='text',
     text='1: Enojo\n2: Asco\n3: Miedo\n4: Felicidad\n5: Trsteza\n6: Sorpresa',
     font='Arial',
-    pos=[0, -0.5], height=0.1, wrapWidth=None, ori=0, 
+    pos=[0, -0.5], height=0.03, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=-4.0);
 text_3 = visual.TextStim(win=win, name='text_3',
     text='Neutral',
     font='Arial',
-    pos=[-0.5, -0.5], height=0.1, wrapWidth=None, ori=0, 
+    pos=[-0.5, -0.5], height=0.03, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=-5.0);
 
 # --- Initialize components for Routine "Thanks" ---
 text_4 = visual.TextStim(win=win, name='text_4',
-    text='Terminaste la tarea! \n\n¡Reclama tu puente en el tablero!',
+    text='Terminaste la tarea\n\n¡Reclama tu puente en el tablero!',
     font='Arial',
-    pos=[0, 0], height=0.1, wrapWidth=None, ori=0, 
+    pos=[0, 0], height=0.03, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=0.0);
@@ -150,6 +157,103 @@ text_4 = visual.TextStim(win=win, name='text_4',
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
 routineTimer = core.Clock()  # to track time remaining of each (possibly non-slip) routine 
+
+# --- Prepare to start Routine "Instr" ---
+continueRoutine = True
+routineForceEnded = False
+# update component parameters for each repeat
+key_resp_3.keys = []
+key_resp_3.rt = []
+_key_resp_3_allKeys = []
+# keep track of which components have finished
+InstrComponents = [text_2, key_resp_3]
+for thisComponent in InstrComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+frameN = -1
+
+# --- Run Routine "Instr" ---
+while continueRoutine:
+    # get current time
+    t = routineTimer.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=routineTimer)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text_2* updates
+    if text_2.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text_2.frameNStart = frameN  # exact frame index
+        text_2.tStart = t  # local t and not account for scr refresh
+        text_2.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text_2, 'tStartRefresh')  # time at next scr refresh
+        # add timestamp to datafile
+        thisExp.timestampOnFlip(win, 'text_2.started')
+        text_2.setAutoDraw(True)
+    
+    # *key_resp_3* updates
+    waitOnFlip = False
+    if key_resp_3.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        key_resp_3.frameNStart = frameN  # exact frame index
+        key_resp_3.tStart = t  # local t and not account for scr refresh
+        key_resp_3.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(key_resp_3, 'tStartRefresh')  # time at next scr refresh
+        # add timestamp to datafile
+        thisExp.timestampOnFlip(win, 'key_resp_3.started')
+        key_resp_3.status = STARTED
+        # keyboard checking is just starting
+        waitOnFlip = True
+        win.callOnFlip(key_resp_3.clock.reset)  # t=0 on next screen flip
+        win.callOnFlip(key_resp_3.clearEvents, eventType='keyboard')  # clear events on next screen flip
+    if key_resp_3.status == STARTED and not waitOnFlip:
+        theseKeys = key_resp_3.getKeys(keyList=['space'], waitRelease=False)
+        _key_resp_3_allKeys.extend(theseKeys)
+        if len(_key_resp_3_allKeys):
+            key_resp_3.keys = _key_resp_3_allKeys[-1].name  # just the last key pressed
+            key_resp_3.rt = _key_resp_3_allKeys[-1].rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        routineForceEnded = True
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in InstrComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# --- Ending Routine "Instr" ---
+for thisComponent in InstrComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# check responses
+if key_resp_3.keys in ['', [], None]:  # No response was made
+    key_resp_3.keys = None
+thisExp.addData('key_resp_3.keys',key_resp_3.keys)
+if key_resp_3.keys != None:  # we had a response
+    thisExp.addData('key_resp_3.rt', key_resp_3.rt)
+thisExp.nextEntry()
+# the Routine "Instr" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
 
 # --- Prepare to start Routine "Instr" ---
 continueRoutine = True
@@ -505,3 +609,5 @@ if eyetracker:
 thisExp.abort()  # or data files will save again on exit
 win.close()
 core.quit()
+
+ioServer.quit()
